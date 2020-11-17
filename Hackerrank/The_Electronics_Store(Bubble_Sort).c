28dde10d4b2de087ce9d2c63d8f2d693d123af5f
Unnati Mishra <54355445+CodesbyUnnati@users.noreply.github.com>
2020-11-17 22:59:44 +0530

    Create The_Electronics_Store(Bubble_Sort).c

---
+++ Hackerrank/The_Electronics_Store(Bubble_Sort).c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int n;
+    scanf("%d",&n);
+    int a[n],i;
+    for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+    int j;
+    for(i=0;i<n-1;i++)
+    {
+        for(j=0;j<n-1-i;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                int t=a[j];
+                a[j]=a[j+1];
+                a[j+1]=t;
+            }
+        }
+    }
+    
+    for(i=0;i<n;i++)
+        printf("%d ",a[i]);
+    
+    return 0;
+}
