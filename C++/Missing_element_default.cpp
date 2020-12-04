4c56775e17410ede436127f4b67c477c39f80834
Unnati Mishra <54355445+CodesbyUnnati@users.noreply.github.com>
2020-12-04 22:34:32 +0530

    Create Missing_element_default.cpp

---
+++ C++/Missing_element_default.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the elements for array: ";
+    cin >> n;
+    int A[10];
+    int sum = 0;
+    if (n == 0 && n == 1)
+    {
+        cout << "Enter bigger size.";
+        
+    }
+    cout << "\nEnter sorted array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> A[i];
+    }
+    //SUM THE ACTUAL ELEMENTS - SUM THE TOTAL ELEMENTS EXCLUDING MISSING
+    for (int i = 0; i < n; i++)
+    {
+        sum = sum + A[i];
+    }
+    int total = n * (n + 1) / 2;
+    cout << "\nThe missing Element found in this sorted array is: " << total - sum;
+
+    return 0;
+}
