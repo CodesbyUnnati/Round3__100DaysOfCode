6bcdd3776e5667740e9259ac8401e802aa38f087
Unnati Mishra <54355445+CodesbyUnnati@users.noreply.github.com>
2020-12-09 22:14:20 +0530

    Create multiple_duplicates_unsorted.cpp

---
+++ C++/multiple_duplicates_unsorted.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, i, j;
+    int A[10];
+    int count;
+    cout << "Enter number: ";
+    cin >> n;
+    cout << "Enter Array: ";
+    for (i = 0; i < n; i++)
+    {
+        cin >> A[i];
+    }
+    for (i = 0; i < n - 1; i++)
+    {
+        count = 1;
+        if (A[i] != -1)
+        {
+            for (j = i + 1; j < n; j++)
+            {
+                if (A[i] == A[j])
+                {
+                    count++;
+                    A[j] = -1;
+                }
+            }
+            if (count > 1)
+            {
+                cout << "Duplicate is: " << A[i] << " it comes " << count << " times.\n";
+            }
+        }
+    }
+    return 0;
+}
